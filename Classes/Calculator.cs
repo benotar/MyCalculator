0f2c694b97e9ae5b5c724d729bc3182fa96c9000
Ivan <zhmur.ivan@gmail.com>
2024-01-18 19:28:11 +0200

    Added logic to the interface and created a Calculator class and implemented the interfaces in it

---
+++ Classes/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using MyCalculator.Interfaces;
+
+namespace MyCalculator.Classes;
+
+public class Calculator : ICalculator
+{
+    public double Addition(double left, double right)
+       => left + right;
+
+
+    public double Subtraction(double left, double right)
+    => left - right;
+
+    public double Multiplication(double left, double right)
+    => left * right;
+
+
+
+    public double Division(double left, double right)
+    {
+        if(right is 0)
+        {
+            throw new ArgumentException("You cannot divide by zero", nameof(right));
+        }
+
+        return left / right;
+    }
+
+}
--- Interfaces/ICalculator.cs
+++ Interfaces/ICalculator.cs
@@ -2,4 +2,8 @@
 
 public interface ICalculator
 {
+    double Addition(double left, double right);
+    double Subtraction(double left, double right);
+    double Multiplication (double left, double right);
+    double Division (double left, double right);
 }
